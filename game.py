0fc962d96a9937da15a4fbfaa2a4c51a943bfd0e
Xpunger <abrombergues643@coralreefhigh.org>
2016-09-02 21:57:13 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,11 @@
+import pygame as pg
+import sys
 
+pg.init()
+screen = pg.display.set_mode([800,600])
+
+while True:
+  for event in pg.event.get():
+    if event.type == pg.QUIT:
+      pg.quit()
+      sys.exit()
